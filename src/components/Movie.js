a4e3135e5baea1f9023ba3a0c2e3bc2c72446b59
Anne Quinkenstein <a.quinkenstein@gmail.com>
2020-12-05 10:36:46 +0100

    Movielist added hover-effect

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -4,30 +4,26 @@ import { Link } from "react-router-dom";
 const Movie = ({ movie, changeFavorites, index }) => {
   console.log("movie", movie);
   return (
-    <div className="container">
-      <div className="box">
+    <div className="box">
       <Link to={`movie/${movie.id}`}>
         <div className="image-container">
-         
-            <img
-              src={movie.posterUrl}
-              alt={movie.title}
-              onError={(e) => {
-                e.target.onerror = null;
-                e.target.parentNode.parentNode.parentNode.className = "noPic";
-              }}
-            />{" "}
-         
-          </div>
-          </Link>
-          <div className="star">
-            <div onClick={() => changeFavorites(movie, index)}>
-              {movie.favorite ? (
-                <i class="fa fa-star"></i>
-              ) : (
-                <i class="fa fa-star-o"></i>
-              )}
-            </div>
+          <img
+            src={movie.posterUrl}
+            alt={movie.title}
+            onError={(e) => {
+              e.target.onerror = null;
+              e.target.parentNode.parentNode.parentNode.className = "noPic";
+            }}
+          />{" "}
+        </div>
+      </Link>
+      <div className="star">
+        <div onClick={() => changeFavorites(movie, index)}>
+          {movie.favorite ? (
+            <i class="fa fa-star"></i>
+          ) : (
+            <i class="fa fa-star-o"></i>
+          )}
         </div>
       </div>
     </div>
