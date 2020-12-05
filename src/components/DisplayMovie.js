6d62f222705e7e0104640e9830e5e9746085addd
Anne Quinkenstein <a.quinkenstein@gmail.com>
2020-12-05 14:55:28 +0100

    changed design of display movie

--- src/components/DisplayMovie.js
+++ src/components/DisplayMovie.js
@@ -4,30 +4,32 @@ import { Link } from "react-router-dom";
 
 const DisplayMovie = ({ data }) => {
   let { id } = useParams();
-  console.log(data);
+
   const matchedMovie = data && data.movies.find((movie) => movie.id === +id);
 
   return (
-    <>
-      <div>
+    <div className="displayMovie">
+      <div className="title">
         <Link to="/">Movielist</Link>
-      </div>
-      <div>
         <h1> {matchedMovie && matchedMovie.title} </h1>
-        <img
-          src={matchedMovie && matchedMovie.posterUrl}
-          alt={matchedMovie && matchedMovie.title}
-        />
       </div>
-      <div className="Infos">
-        <p> Year: {matchedMovie && matchedMovie.year} </p>
-        <p> Director: {matchedMovie && matchedMovie.director} </p>
-        <p> Actors: {matchedMovie && matchedMovie.actors} </p>
-        <p> Genres: {matchedMovie && matchedMovie.genres} </p>
-        <p> Plot: {matchedMovie && matchedMovie.plot} </p>
-        <p> Rutime: {matchedMovie && matchedMovie.runtime} </p>
+      <div className="movieInfos">
+        <div className="poster">
+          <img
+            src={matchedMovie && matchedMovie.posterUrl}
+            alt={matchedMovie && matchedMovie.title}
+          />
+        </div>
+        <div className="infos">
+          <p> Year: {matchedMovie && matchedMovie.year} </p>
+          <p> Director: {matchedMovie && matchedMovie.director} </p>
+          <p> Actors: {matchedMovie && matchedMovie.actors} </p>
+          <p> Genres: {matchedMovie && matchedMovie.genres} </p>
+          <p> Plot: {matchedMovie && matchedMovie.plot} </p>
+          <p> Rutime: {matchedMovie && matchedMovie.runtime} </p>
+        </div>
       </div>
-    </>
+    </div>
   );
 };
 
