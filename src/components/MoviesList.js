82b1db5e63eb90d59baf8bf1cdfdcb1f35a636ec
Anne Quinkenstein <a.quinkenstein@gmail.com>
2020-12-05 18:02:38 +0100

    background stars

--- src/components/MoviesList.js
+++ src/components/MoviesList.js
@@ -1,16 +1,26 @@
-import React from 'react';
-import Movie from './Movie'; 
-
+import React from "react";
+import Movie from "./Movie";
 
 const MoviesList = ({ data, changeFavorites, render }) => {
-
-  data &&  data.movies.sort((a, b)=> (b.favorite || false) - (a.favorite || false))
+  data &&
+    data.movies.sort((a, b) => (b.favorite || false) - (a.favorite || false));
 
   return (
     <div className="App">
-      {data && data.movies.map((movie, index) => <Movie movie={movie} key={movie.id} index={index} changeFavorites ={changeFavorites}/> )}
+      <div id="stars"></div>
+      <div id="stars2"></div>
+      <div id="stars3"></div>
+      {data &&
+        data.movies.map((movie, index) => (
+          <Movie
+            movie={movie}
+            key={movie.id}
+            index={index}
+            changeFavorites={changeFavorites}
+          />
+        ))}
     </div>
   );
-}
+};
 
 export default MoviesList;
